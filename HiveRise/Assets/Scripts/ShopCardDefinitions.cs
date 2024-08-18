b5bf75312fda1e9cca127c34f6352dae1991157b
Michael Wolf <michaelwolf95@gmail.com>
2024-08-18 11:24:45 -0700

    Shop data parser

--- HiveRise/Assets/Scripts/ShopCardDefinitions.cs
+++ HiveRise/Assets/Scripts/ShopCardDefinitions.cs
@@ -35,6 +35,24 @@ namespace HiveRise
 
 			// ToDo: Parse cards.
 
+			string[] data = shopDataCSV.text.Split(new string[] {"\n"}, StringSplitOptions.None);
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				string[] dataEntries = data[i].Split(new string[] {","}, StringSplitOptions.None);
+				
+				ShopCardData shopCardData = new ShopCardData();
+				shopCardData.cardData = new CardData();
+
+				shopCardData.cost = int.Parse(dataEntries[0]);
+				shopCardData.cardData.pieceShapeID = int.Parse(dataEntries[1]);
+				shopCardData.cardData.rarity = (CardRarity)int.Parse(dataEntries[2]);
+				shopCardData.cardData.color = (PieceColor)int.Parse(dataEntries[3]);
+				shopCardData.cardData.weightType = (WeightType)int.Parse(dataEntries[4]);
+				shopCardData.cardData.scoreMultiplier = (ScoreMultiplier)int.Parse(dataEntries[5]);
+				shopCardData.cardData.sticky = int.Parse(dataEntries[6]) == 1;
+			}
+
 		}
 	}
 }
