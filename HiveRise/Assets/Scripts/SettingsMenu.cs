54a9c305de782e2898690379b0f87d703753a0ab
Heather Wright <heathertatianawright@gmail.com>
2024-08-19 22:25:31 -0700

    Sprite and UI tweaks

--- HiveRise/Assets/Scripts/SettingsMenu.cs
+++ HiveRise/Assets/Scripts/SettingsMenu.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class SettingsMenu : MonoBehaviour
 { 
     public GameObject Panel;
-
+    public GameObject UIPanel;
     public GameObject Cog;
 
     public float xAngle, yAngle, zAngle;
 
     public void openPanel()
     {
-        if(Panel !=null)
+        if (Panel != null)
         {
             bool isActive = Panel.activeSelf;
 
             Panel.SetActive(!isActive);
-            Cog.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
         }
+        if (UIPanel != null)
+        {
+            bool isActive = UIPanel.activeSelf;
+
+            UIPanel.SetActive(!isActive);
+        }
+
+        Cog.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
     }
     
 }
